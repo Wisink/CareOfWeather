08b4d3908775582e50b769f33fa337e662560522
Wisink <wangzheweixin@foxmail.com>
2017-09-29 16:42:34 +0800

    单位提交：编写选择省市县代码

---
+++ app/src/main/java/com/careofweather/android/ChooseAreaFragment.java
@@ -0,0 +1,84 @@
+package com.careofweather.android;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.careofweather.android.db.City;
+import com.careofweather.android.db.County;
+import com.careofweather.android.db.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChooseAreaFragment extends Fragment {
+
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+
+    /**
+     * 省列表
+     */
+    private List<Province> provinceList;
+
+    /**
+     * 市列表
+     */
+    private List<City> cityList;
+
+    /**
+     * 县列表
+     */
+    private List<County> countyList;
+
+    /**
+     * 选中的省份
+     */
+    private Province selectedProvince;
+
+    /**
+     * 选中的城市
+     */
+    private City selectedCity;
+
+    /**
+     * 当前选中的级别
+     */
+    private int currentLevel;
+
+    public ChooseAreaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+}
